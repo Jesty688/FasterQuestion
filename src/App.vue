2e153fa4e1115cf162f38c90f99ea9a396f63678
jesty688 <1980963713@qq.com>
2020-12-04 12:46:15 +0800

    优化顶栏

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <template>
   <v-app>
-    <nav>
-      <v-app-bar color="#fff" class="elevation">
-        <v-app-bar-nav-icon
-          @click="drawer = drawer ? false : true"
-        ></v-app-bar-nav-icon>
-        <a href="#" class="toolbar_title">
-          <v-toolbar-title>FlashQ</v-toolbar-title>
-        </a>
-        <v-spacer></v-spacer>
-      </v-app-bar>
-    </nav>
-
-    <v-navigation-drawer class="nav_asideg" v-model="drawer">
-      <h2>12</h2>
+    <v-app-bar color="#fff" class="elevation" light fixed>
+      <v-app-bar-nav-icon
+        @click="drawer = drawer ? false : true"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title class="toolbar_title">FastQs</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-navigation-drawer app absolute v-model="drawer">
+      <h1>cb</h1>
     </v-navigation-drawer>
+    <v-main class="content_top">
+      <v-container fluid>
+        <h1>12312ssss2313123123123312</h1>
+        <div>123ccccsfdsjflksjfskjflksj</div>
+      </v-container>
+    </v-main>
   </v-app>
 </template>
 <script>
@@ -23,22 +27,29 @@ export default {
     drawer: false,
   }),
   mounted() {
-    console.log(
-      (document.querySelector(".v-navigation-drawer__border").style.display =
-        "none")
-    );
+    // console.log(
+    //   (document.querySelector(".v-navigation-drawer__border").style.display =
+    //     "none")
+    // );
   },
   name: "App",
 };
 </script>
 <style scoped>
 @import "assets/css/reset.css";
+.fixed {
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+}
 .elevation {
   box-shadow: 0 1px 6px 0 rgba(32, 33, 36, 0.28) !important;
 }
-.nav_asideg {
-  margin: 8px 0;
+.content_top {
+  padding-top: 64px !important;
 }
+
 .toolbar_title {
   font-size: 22px !important;
   color: inherit;
@@ -46,6 +57,7 @@ export default {
   -ms-user-select: none;
   user-select: none;
   margin: 0 16px;
+  font-family: "Menlo", "PingFang" !important;
 }
 .toolbar_title > div {
   font-family: "Menlo", "PingFang" !important;
